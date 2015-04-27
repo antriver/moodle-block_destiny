d8ec77ed52fad42e3c5bc44b79634c63ab3f7360
Anthony Kuske <anthonykuske@gmail.com>
2015-04-27 17:02:52 +0200

    Cleanup class file

--- block_destiny.php
+++ block_destiny.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Main plugin functions for destiny block
+ *
  * @package    block_destiny
  * @copyright  Anthony Kuske <www.anthonykuske.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -8,20 +10,44 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-class block_destiny extends block_base {
-
-    public function init() {
+class block_destiny extends block_base
+{
+    public function init()
+    {
         $this->title = get_string('pluginname', 'block_destiny');
     }
 
-    public function get_content() {
+    /**
+     * Returns the contents when shown in block form
+     */
+    public function get_content()
+    {
         return '';
     }
 
+    /**
+     * Define what pages the block can be added to
+     * (We only want to view the block at /blocks/destiny but something needs
+     * to be defined here)
+     */
+    public function applicable_formats()
+    {
+        return array(
+            'all' => false,
+            'site' => true,
+        );
+    }
+
+    /**
+     * Allow multiple instances of the block on the same page?
+     */
     public function instance_allow_multiple() {
         return false;
     }
 
+    /**
+     * Does the block have admin settings?
+     */
     public function has_config() {
         return true;
     }
