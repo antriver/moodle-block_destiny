1ab6cfb2509cee66d6adb59995b96bdb58ef0dbd
Anthony Kuske <anthonykuske@gmail.com>
2015-04-28 20:53:06 +0200

    Update required moodle version

--- version.php
+++ version.php
@@ -6,6 +6,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $plugin->version = 2015042600;
-$plugin->requires = 2013051404;   // Moodle 2.5 January
+$plugin->requires = 2013111800; // MOODLE_26_STABLE
 $plugin->component = 'block_destiny';
